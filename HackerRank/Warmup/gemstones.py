02fb0a14586b22a7ca81c67a68ca0869cd64d4a0
Sanal Kumar <sanaltsk@gmail.com>
2014-09-06 07:34:27 +0530

    Changing Dir Structure

--- HackerRank/Warmup/gemstones.py
+++ HackerRank/Warmup/gemstones.py
@@ -1,4 +1,4 @@
-# Gem Stones (https://www.hackerrank.com/challenges/gem-stones) 
+# Gem Stones (https://www.hackerrank.com/challenges/gem-stones)
 # John has discovered various rocks. Each rock is composed of various elements, and each element is represented by a lowercase latin letter from 'a' to 'z'. An element can be present multiple times in a rock. An element is called a 'gem-element' if it occurs at least once in each of the rocks.
 #
 # Given the list of N rocks with their compositions, display the number of gem-elements that exist in those rocks.
